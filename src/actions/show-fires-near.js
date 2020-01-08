80763150f6563431dbb5ddb8f6813b6dedff181f
Alexander Ruberto <rubertoalexander@gmail.com>
2020-01-08 21:13:40 +1100

    Getting distance to nearby fires
    Getting null error due to promise and not updating states

--- src/actions/show-fires-near.js
+++ src/actions/show-fires-near.js
@@ -1,6 +1,20 @@
 import React from 'react'
 import { Text } from '@botonic/react'
 import { RequestContext } from '@botonic/react'
+import getDistance from 'geolib/es/getDistance';
+
+const fireLocations = [
+    {lat: -37.0491119, long: 145.9573896},
+    {lat: -36.9861296, long: 147.2673626},
+    {lat: -37.3517694, long: 147.5290855},
+    {lat: -37.2383802, long: 148.844171},
+    {lat: -36.0929461, long: 147.1619736},
+    {lat: -35.9469816, long: 147.8611158},
+    {lat: -35.6828146, long: 148.0427962},
+    {lat: -36.0073702, long: 148.6318722},
+    {lat: -37.323187, long: 149.6667035},
+    {lat: -36.5166649, long: 149.4824904}
+    ];
 
 export default class extends React.Component {
 
@@ -8,8 +22,15 @@ export default class extends React.Component {
         super(props)
         this.state = {
             lat: null,
-            long: null
+            long: null,
+            fireDist: null
         }
+
+        this.getPosition().then( () => {
+            console.log("here");
+
+
+        });
     }
 
     static contextType = RequestContext
@@ -17,7 +38,7 @@ export default class extends React.Component {
         return { input }
     }
 
-    position = async () => {
+    getPosition = async () => {
         await navigator.geolocation.getCurrentPosition(
             position => this.setState({
                 lat: position.coords.latitude,
@@ -25,15 +46,48 @@ export default class extends React.Component {
             }),
             err => console.log(err)
         );
-        console.log(this.state.lat, this.state.long)
+        await console.log(this.state.lat, this.state.long);
+        await this.setState({fireDist: this.getClosestDistance()});
+    }
+
+    /*
+        Traverses array of coordinates and returns the closest distance to a fire for the user
+     */
+    getClosestDistance = () => {
+        let i = 1;
+        let closestDistance = fireLocations[0];
+        while (i < fireLocations.length) {
+
+            console.log(fireLocations[i]);
+
+            let measuredDistance = getDistance(
+                {latitude: this.state.lat, longitude: this.state.long},
+                {latitude: fireLocations[i].lat, longitude: fireLocations[i].long});
+
+            if (measuredDistance < closestDistance) {
+                closestDistance = measuredDistance;
+            }
+
+            i++;
+        }
+
+        return closestDistance;
     }
 
     render() {
-        this.position();
+            /*this.getPosition().then( () => {
+                console.log("here");
+                this.setState({fireDist: this.getClosestDistance()});
+                console.log("out")
+                console.log(this.state.fireDist);
+        });*/
         return (
             <>
+
                 <Text>To see if there are fires near you, let me get your location first...</Text>
                 <Text>You are at {this.state.lat + ", " + this.state.long}</Text>
+                <Text>The closest fire to you is {this.state.fireDist} km away.</Text>
+
             </>
         )
     }
