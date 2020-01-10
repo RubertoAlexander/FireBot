87637180a38e1565c0cd728d1da17c4c21864682
Alexander Ruberto <rubertoalexander@gmail.com>
2020-01-10 15:07:05 +1100

    Explain safety

---
+++ src/actions/link-safety.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Text, Video, Document, Element, Button} from '@botonic/react'
+import { RequestContext } from '@botonic/react'
+
+export default class extends React.Component {
+    static contextType = RequestContext
+
+    render() {
+        return (
+            <>
+                <Text>Please see the following information to be safe, we have a video and a survival plan for you to look through.</Text>
+                <div style={{marginLeft: "10%"}}>
+                    <Element>
+                        <iframe width="200" height="125" src="https://www.youtube.com/embed/o2YgHWnw2Yo" frameBorder="0"
+                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen/>
+                    </Element>
+                </div>
+                <Button url={"https://www.rfs.nsw.gov.au/__data/assets/pdf_file/0003/36597/GetReadyforaBushFire.pdf"}>Bushfire Survival Plan</Button>
+            </>
+        )
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,19 @@ import GetLocation from './actions/get-location'
 import FiresNear from './actions/show-fires-near'
 import SayGoodbye from './actions/say-goodbye'
 import NotFound from './actions/not-found'
+import SafetyAdvice from './actions/link-safety'
 
 export const routes = [
-  { input: i => i.confidence < 0.7, action: NotFound },
-  { intent: 'Greetings', action: Start },
-  { intent: 'Job', action: ExplainJob},
-  { path: 'firesNear', intent: 'FiresNear', action: GetLocation,
+  {input: i => i.confidence < 0.7, action: NotFound},
+  {path: 'start', intent: 'Greetings', action: Start},
+  {path: 'explainJob', intent: 'Job', action: ExplainJob},
+  {path: "safetyAdvice", intent: "BeSafe", action: SafetyAdvice},
+  {path: 'firesNear', intent: 'FiresNear', action: GetLocation,
     childRoutes: [
       {path: "get-location", payload: "Melbourne", action: FiresNear},
       {path: "get-location", payload: "Sydney", action: FiresNear},
     ]
   },
-  { intent: 'Gratitude', action: SayGoodbye }
+  { path: 'bye', intent: 'Gratitude', action: SayGoodbye},
+
 ]
