bcb01bf1ee22151477cd3818693f9df150975e4d
Alexander Ruberto <rubertoalexander@gmail.com>
2020-01-10 16:13:34 +1100

    Routes and models updated

--- src/routes.js
+++ src/routes.js
@@ -2,21 +2,24 @@ import Start from './actions/start'
 import ExplainJob from './actions/explain-job'
 import GetLocation from './actions/get-location'
 import FiresNear from './actions/show-fires-near'
-import SayGoodbye from './actions/say-goodbye'
+import SayThanks from './actions/say-thanks'
 import NotFound from './actions/not-found'
 import SafetyAdvice from './actions/link-safety'
+import ShowFires from './actions/show-fire-map'
+import Goodbye from './actions/say-goodbye'
 
 export const routes = [
   {input: i => i.confidence < 0.7, action: NotFound},
   {path: 'start', intent: 'Greetings', action: Start},
   {path: 'explainJob', intent: 'Job', action: ExplainJob},
   {path: "safetyAdvice", intent: "BeSafe", action: SafetyAdvice},
+  {path: "showFires", intent: "WhereFires", action: ShowFires},
   {path: 'firesNear', intent: 'FiresNear', action: GetLocation,
     childRoutes: [
       {path: "get-location", payload: "Melbourne", action: FiresNear},
       {path: "get-location", payload: "Sydney", action: FiresNear},
     ]
   },
-  { path: 'bye', intent: 'Gratitude', action: SayGoodbye},
-
+  { path: 'Thanks', intent: 'Gratitude', action: SayThanks},
+  {path: 'Bye', intent: 'Bye', action: Goodbye}
 ]
